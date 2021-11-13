7c19dfb781b5b1b7242b6cb0584362ea8c87c113
Warren Dugan <warrendugan@GMAIL.COM>
2021-11-12 21:45:00 -0800

    adds www app

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'jankless-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
